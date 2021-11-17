1e65202498bf04b43167c738f1f0773bba512303
zm1ca <zmicier.fied@yandex.ru>
2021-11-17 10:50:46 +0300

    feat: set up tappable search button with activity indicator

---
+++ GoogleSearchTask/Custom/GSSearchButton.swift
@@ -0,0 +1,45 @@
+//
+//  GSSearchButton.swift
+//  GoogleSearchTask
+//
+//  Created by Źmicier Fiedčanka on 17.11.21.
+//
+
+import UIKit
+
+class GSSearchButton: UIButton {
+    
+    var appearance: SearchButtonAppearance {
+        didSet { updateAppearance() }
+    }
+
+    override init(frame: CGRect) {
+        appearance = .search
+        
+        super.init(frame: .zero)
+        translatesAutoresizingMaskIntoConstraints = false
+        updateAppearance()
+        setTitleColor(.GSDarkSky, for: .normal)
+        layer.cornerRadius = 12
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func updateAppearance() {
+        switch appearance {
+        case .search:
+            backgroundColor = .systemGreen
+            setTitle("Search", for: .normal)
+        case .stop:
+            backgroundColor = .systemRed
+            setTitle("Stop", for: .normal)
+        }
+    }
+    
+}
+
+enum SearchButtonAppearance {
+    case search, stop
+}
--- GoogleSearchTask/SearchVC.swift
+++ GoogleSearchTask/SearchVC.swift
@@ -9,14 +9,32 @@ import UIKit
 
 class SearchVC: UIViewController {
     
-    private let queryTextField = GSTextField(placeholderText: "Who is John Doe?", accentColor: .GSDarkSky)
+    private var isSearching: Bool! {
+        didSet {
+            if isSearching {
+                searchButton.appearance = .stop
+                activityIndicator.startAnimating()
+            } else {
+                searchButton.appearance = .search
+                activityIndicator.stopAnimating()
+            }
+        }
+    }
+    
+    private let queryTextField    = GSTextField(placeholderText: "Who is John Doe?", accentColor: .GSDarkSky)
+    private let searchButton      = GSSearchButton()
+    private let activityIndicator = UIActivityIndicatorView(style: .large)
     
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        configureHidingKeyboardOnTap()
         layoutUI()
+        
+        configureHidingKeyboardOnTap()
+        configureButtonActions()
+        
+        isSearching = false
     }
     
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
@@ -26,17 +44,28 @@ class SearchVC: UIViewController {
     
     //MARK: - UI
     private func layoutUI() {
-        view.addSubviews(queryTextField)
+        
+        view.addSubviews(queryTextField, searchButton, activityIndicator)
+        
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             queryTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
             queryTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
             queryTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8),
-            queryTextField.heightAnchor.constraint(equalToConstant: 50)
+            queryTextField.heightAnchor.constraint(equalToConstant: 50),
+            
+            searchButton.topAnchor.constraint(equalTo: queryTextField.bottomAnchor, constant: 8),
+            searchButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
+            searchButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8),
+            searchButton.heightAnchor.constraint(equalToConstant: 50),
+            
+            activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         ])
     }
     
     
-    //MARK: - Configurations
+    //MARK: - Configure Text Field
     private func configureHidingKeyboardOnTap() {
         queryTextField.delegate = self
         
@@ -47,13 +76,25 @@ class SearchVC: UIViewController {
     @objc private func hideKeyboard() {
         view.endEditing(true)
     }
+    
+    
+    //MARK: - Configure Button
+    private func configureButtonActions() {
+        searchButton.addTarget(self, action: #selector(searchButtonTapped), for: .touchUpInside)
+    }
+    
+    @objc private func searchButtonTapped() {
+        isSearching.toggle()
+        hideKeyboard()
+        print("Perform search")
+    }
 }
 
 
 extension SearchVC: UITextFieldDelegate {
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        hideKeyboard()
+        searchButtonTapped()
         return true
     }
 }
