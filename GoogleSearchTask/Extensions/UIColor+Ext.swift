1070f9b3530cdb41b0f16c6c1d37114ef759fb7a
zm1ca <zmicier.fied@yandex.ru>
2021-11-19 14:26:19 +0300

    fix: color naming issue

--- GoogleSearchTask/Extensions/UIColor+Ext.swift
+++ GoogleSearchTask/Extensions/UIColor+Ext.swift
@@ -13,6 +13,6 @@ extension UIColor {
     static let palCerulean  = UIColor(named: "PaleCerulean")!
     static let lightCyan    = UIColor(named: "LightCyan")!
     static let burntSienna  = UIColor(named: "BurntSienna")!
-    static let gunmetal     = UIColor(named: "Gunmetal")!
+    static let gunmetal     = UIColor(named: "GunMetal")!
 }
 
