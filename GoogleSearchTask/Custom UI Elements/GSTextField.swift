00d40e77bb7ad925c0b559e1bcbc19ce66e0ff78
zm1ca <zmicier.fied@yandex.ru>
2021-11-19 12:12:36 +0300

    refactor: tweak text field

--- GoogleSearchTask/Custom UI Elements/GSTextField.swift
+++ GoogleSearchTask/Custom UI Elements/GSTextField.swift
@@ -25,17 +25,33 @@ class GSTextField: UITextField {
         layer.borderColor       = UIColor.lightGray.cgColor
         layer.borderWidth       = 2
         layer.cornerRadius      = 12
-        layer.sublayerTransform = CATransform3DMakeTranslation(16, 0, 0)
         
         autocorrectionType      = .no
         autocapitalizationType  = .sentences
         returnKeyType           = .google
+        clearButtonMode         = .always
         
         translatesAutoresizingMaskIntoConstraints = false
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    //MARK: - Insets
+    let padding = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 5)
+    
+    override open func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+    
+    override open func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+    
+    override open func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
     }
 
 }
